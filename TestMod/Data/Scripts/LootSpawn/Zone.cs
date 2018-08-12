3ef6b2fb8458bc302f42aa7e49d860836203f380
brx <dev.brax@gmail.com>
2018-08-11 21:01:23 -0400

    Initial commit

---
+++ TestMod/Data/Scripts/LootSpawn/Zone.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VRage.Game.ModAPI;
+using VRageMath;
+using Sandbox.ModAPI;
+
+namespace TestMod.LootSpawn
+{
+    public class Zone
+    {
+        private string _zoneName { get; set; }
+        private BoundingSphereD _zoneBounds { get; set; }
+        private bool _hasGPS { get; set; }
+        private IMyGps _GPS { get; set; }
+        public List<long> _lastFoundPlayers { get; set; }
+
+        private void CreateGPS(Vector3D position, string gpsName)
+        {
+            _GPS = MyAPIGateway.Session.GPS.Create(gpsName,
+                                                     "GPS",
+                                                     position,
+                                                     true, true);
+
+            MyAPIGateway.Session.GPS.AddLocalGps(_GPS);
+        }
+
+        private void UpdateStatus()
+        {
+            _GPS.Name = $"{_zoneName} - P:{_lastFoundPlayers.Count}";
+
+            //Get all players
+            var players = new List<IMyPlayer>();
+            MyAPIGateway.Players.GetPlayers(players);
+
+            //Modify existing GPS
+            foreach (var player in players)
+                MyAPIGateway.Session.GPS.ModifyGps(player.IdentityId, _GPS);
+        }
+
+        public Zone(string zoneName, Vector3D position, double radius, bool createGPS = false)
+        {
+            _zoneName = zoneName;
+            _zoneBounds = new BoundingSphereD(position, (double)radius);
+
+            _hasGPS = createGPS;
+
+            if (createGPS)
+                CreateGPS(position, zoneName);
+        }
+        public Zone(string zoneName, double x, double y, double z, double radius, bool createGPS = false)
+        {
+            var position = new Vector3D(x, y, z);
+            _zoneName = zoneName;
+            _zoneBounds = new BoundingSphereD(position, (double)radius);
+
+            _hasGPS = createGPS;
+
+            if (createGPS)
+                CreateGPS(position, zoneName);
+        }
+
+        public void Update()
+        {
+            var bounds = _zoneBounds;
+            var allEntities = MyAPIGateway.Entities.GetTopMostEntitiesInSphere(ref bounds);
+            var shipControllers = allEntities.Where(e => e is Sandbox.ModAPI.IMyShipController).Select(s => (Sandbox.ModAPI.IMyShipController)s);
+
+            var pilots = shipControllers.Where(s =>
+                s.IsUnderControl &&
+                s.ControllerInfo.IsLocallyHumanControlled() &&
+                s.ControllerInfo.ControllingIdentityId > 0)
+                .Select(s => s.ControllerInfo.ControllingIdentityId);
+
+            _lastFoundPlayers = pilots.ToList();
+
+            if (_hasGPS)
+                UpdateStatus();
+        }
+    }
+}
---
+++ TestMod/Data/Scripts/TestMod.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Common;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.Components.Session;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.Utils;
+using VRageMath;
+using TestMod.LootSpawn;
+using Sandbox.ModAPI;
+using ProtoBuf;
+
+using MyAPIGateway = Sandbox.ModAPI.MyAPIGateway;
+
+namespace TestMod
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+    public class TestMod : MySessionComponentBase
+    {
+        int counter = 0;
+        bool debugMode = true;
+        Zone z = new Zone("Test Zone", 0, 0, 0, 100, true);
+
+        public bool _isInitialized { get; set; }
+
+        public TestMod()
+        {
+            LogEntry("TestMod Created!");
+        }
+
+        public override void BeforeStart()
+        {
+            LogEntry("TestMod:BeforeStart called");
+
+            base.BeforeStart();
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+            if (!_isInitialized && MyAPIGateway.Session != null && MyAPIGateway.Session.Player != null)
+            {
+                //Debug = MyAPIGateway.Session.Player.IsExperimentalCreator();
+
+                if (!MyAPIGateway.Session.OnlineMode.Equals(MyOnlineModeEnum.OFFLINE) && MyAPIGateway.Multiplayer.IsServer && !MyAPIGateway.Utilities.IsDedicated)
+                    InitServer();
+                Init();
+            }
+            if (!_isInitialized && MyAPIGateway.Utilities != null && MyAPIGateway.Multiplayer != null
+                && MyAPIGateway.Session != null && MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+            {
+                InitServer();
+                return;
+            }
+
+
+            if(counter % 10 == 0)
+            {
+                z.Update();
+                var players = z._lastFoundPlayers;
+                LogEntry("TestMod Tick");
+            }
+            counter++;
+
+            base.UpdateBeforeSimulation();
+        }
+
+        private void Init()
+        {
+            _isInitialized = true; // Set this first to block any other calls from UpdateBeforeSimulation().
+            LogEntry("TestMod Client Initialized");
+        }
+
+        private void InitServer()
+        {
+            _isInitialized = true;
+            LogEntry("Server started");
+
+            Sandbox.Game.MyVisualScriptLogicProvider.PlayerConnected += PlayerConnected;
+            Sandbox.Game.MyVisualScriptLogicProvider.PlayerDropped += PlayerDropped;
+            Sandbox.Game.MyVisualScriptLogicProvider.PlayerDisconnected += PlayerDisconnected;
+        }
+
+        private void PlayerConnected(long playerId)
+        {
+
+        }
+
+        private void PlayerDropped(string itemTypeName, string itemSubTypeName, long playerId, int amount)
+        {
+
+        }
+
+        private void PlayerDisconnected(long playerId)
+        {
+
+        }
+
+        public override void Init(MyObjectBuilder_SessionComponent sessionComponent)
+        {
+            LogEntry("TestMod:Init called");
+
+            base.Init(sessionComponent);
+        }
+
+        public void LogEntry(string argument)
+        {
+            if (argument.Contains("Debug") == true && debugMode == false)
+                return;
+
+            MyLog.Default.WriteLineAndConsole("TestMod: " + argument);
+
+            if (debugMode == true)
+                MyVisualScriptLogicProvider.ShowNotificationToAll(argument, 5000, "White");
+        }
+    }
+}
